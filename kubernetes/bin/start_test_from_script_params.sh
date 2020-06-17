262bfe6aaf22f28ac316ce2d154eb1205adb872e
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-06-17 14:18:44 +0200

    fix sampler sub steps error

--- kubernetes/bin/start_test_from_script_params.sh
+++ kubernetes/bin/start_test_from_script_params.sh
@@ -33,6 +33,7 @@ echo "Jmeter args $jmeter_args"
 
 kubectl exec -ti -n $tenant $master_pod -- rm -Rf "$tmp"
 kubectl exec -ti -n $tenant $master_pod -- mkdir -p "$tmp/$report_dir"
-kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test "$test_name -l $tmp/results.csv -e -Gthreads=$threads $jmeter_args -o $tmp/$report_dir"
+kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test "$test_name -Jwebdriver.sampleresult_class=com.googlecode.jmeter.plugins.webdriver.sampler.SampleResultWithSubs -l $tmp/results.csv -e -Gthreads=$threads $jmeter_args -o $tmp/$report_dir"
 kubectl cp "$tenant/$master_pod:$tmp/$report_dir" "$local_report_dir"
 
+#enter master pod - kubectl exec -ti -n jmeter jmeter-master-5757fb465-9w4j7 -- bash
